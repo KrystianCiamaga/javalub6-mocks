2fd1cb9f4261cec0d0039aabeb24f3b4b7c2735c
kryspin <krystianciamaga@gmail.com>
2018-03-20 21:56:59 +0100

    No working :(

--- src/main/java/com/demo/camera/PhotoCamera.java
+++ src/main/java/com/demo/camera/PhotoCamera.java
@@ -1,17 +1,78 @@
 package com.demo.camera;
 
-public class PhotoCamera {
+public class PhotoCamera implements WriteListener {
+
+    private boolean OnOf;
+    ImageSensor sensor;
+    Card karta;
+    boolean zapis = false;
+
+
+    public PhotoCamera() {
+
+        OnOf = false;
+    }
+
+
+    public PhotoCamera(ImageSensor sensor) {
+
+        OnOf = false;
+        this.sensor = sensor;
+
+    }
+
+    public PhotoCamera(ImageSensor sensor, Card karta) {
+
+        OnOf = false;
+        this.sensor = sensor;
+        this.karta = karta;
+
+    }
+
 
     public void turnOn() {
-        // not implemented
+
+        OnOf = true;
+        sensor.turnOn();
+
     }
 
     public void turnOff() {
-        // not implemented
+
+        OnOf = false;
+        sensor.turnOff();
+
     }
 
     public void pressButton() {
-        // not implemented
+
+
+        if (OnOf) {
+
+
+            while (!zapis) {
+                karta.write(sensor.read());
+
+            }
+
+        }
     }
+
+
+        public boolean isOnOf () {
+            return OnOf;
+        }
+
+        public void setOnOf ( boolean onOf){
+            OnOf = onOf;
+        }
+
+        @Override
+        public void writeCompleted () {
+
+            zapis = true;
+
+        }
+
 }
 
--- src/test/java/com/demo/camera/PhotoCameraTest.java
+++ src/test/java/com/demo/camera/PhotoCameraTest.java
@@ -1,14 +1,90 @@
 package com.demo.camera;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PhotoCameraTest {
 
 
+
     @Test
-    public void changeName() {
-        Assertions.assertThat(this).isNotNull();
+    public void WhenYouTurnOnCameraSensorOn(){
+
+        ImageSensor sensor=mock(ImageSensor.class);
+        PhotoCamera camera=new PhotoCamera(sensor);
+
+        camera.turnOn();
+
+        Mockito.verify(sensor).turnOn();
+
     }
+
+
+    @Test
+    public void WhenYouTurnOffCameraSensorOff(){
+
+        ImageSensor sensor=mock(ImageSensor.class);
+        PhotoCamera camera=new PhotoCamera(sensor);
+
+        camera.turnOff();
+
+        Mockito.verify(sensor).turnOff();
+
+    }
+
+
+   @Test
+    public void WhenCameraIsTurnOFFPressButtonDoNothing(){
+
+        ImageSensor sensor=mock(ImageSensor.class);
+        PhotoCamera camera=new PhotoCamera(sensor);
+
+
+        camera.pressButton();
+
+        Mockito.verifyZeroInteractions(sensor);
+
+    }
+
+   @Test
+    public void WhenCameraIsOnPressButtonCopy(){
+
+        Card karta=mock(Card.class);
+        ImageSensor sensor=mock(ImageSensor.class);
+
+        PhotoCamera camera=new PhotoCamera(sensor,karta);
+        camera.turnOn();
+        camera.pressButton();
+
+        Mockito.verify(karta).write(Mockito.any());
+
+    }
+
+
+    @Test
+    public void DuringSavingDataTurningOffCameraIsImpossible(){
+
+        Card karta=mock(Card.class);
+        ImageSensor sensor=mock(ImageSensor.class);
+
+        PhotoCamera camera=new PhotoCamera(sensor,karta);
+
+        camera.turnOn();
+        camera.pressButton();
+        camera.turnOff();
+
+        Mockito.verify(sensor).turnOn();
+
+    }
+
+
+
+
 
 }
